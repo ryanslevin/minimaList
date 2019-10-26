1c93d070d8d46a84279f9cb07541d2681ac51553
ryanslevin <ryanslevin@gmail.com>
2019-10-26 10:38:26 -0700

    Set up base folder structure and projects

---
+++ backend/src/main/java/com/ryanslevin/minimalist/MinimalistApplication.java
@@ -0,0 +1,13 @@
+package com.ryanslevin.minimalist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinimalistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinimalistApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ryanslevin/minimalist/MinimalistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryanslevin.minimalist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinimalistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
